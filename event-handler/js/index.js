58066e79358965f734acce2964f40106ee6c83b8
AJ <aj8338@gmail.com>
2021-04-30 01:41:40 +0800

    2159

--- event-handler/js/index.js
+++ event-handler/js/index.js
@@ -2,7 +2,7 @@ function transformUpperCase(_val){
     return _val.toUpperCase();
 }
 
-var elNamaPengguna = document.getElementById("NamaPengguna"); //1 select element
+let elNamaPengguna = document.getElementById("NamaPengguna"); //1 select element
 elNamaPengguna .addEventListener("keyup",function(){
     elNamaPengguna.value = transformUpperCase(elNamaPengguna.value);
 })
