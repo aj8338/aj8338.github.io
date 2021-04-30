ded129045542dc93b4d33adfd651fc345ccda6dc
AJ <aj8338@gmail.com>
2021-04-30 20:12:25 +0800

    812

--- tableapp/js/book.js
+++ tableapp/js/book.js
@@ -2,12 +2,12 @@ let bookNowbtn = document.getElementById("bookNow");
 bookNowbtn.addEventListener("click",function(){
     let userName = document.getElementById("userName");
     let userNameVal= userName.value;
+
     let userEmail = document.getElementById("userEmail");
     let userEmailVal= userEmail.value;
-    let userPax = document.getElementById("userPax");
-    let userPaxVal= userPax.value
 
-    let userRemarks=document.getElementById("userRemarks").value
+    let userPax = document.getElementById("userPax").value;
+    let userRemarks=document.getElementById("userRemarks").value;
 
     BookNow(userNameVal,userEmailVal,userPaxVal,UserRemarksVal);
 
